fca2d2ac82cc73a2f8fd71f953189ac80de582fb
Artem Balamutyuk <artyom.balamutyuk1@gmail.com>
2023-11-29 00:45:16 +0300

    Add random items

--- src/App.vue
+++ src/App.vue
@@ -1,105 +1,19 @@
 <script setup lang="ts">
-const categories = [{
-  id: 1,
-  name: 'Category 1',
-  items: [{
-    id: 1,
-    name: 'Item 1',
-  }, {
-    id: 2,
-    name: 'Item 2',
-  }],
-}, {
-  id: 2,
-  name: 'Category 2',
-  items: [{
-    id: 3,
-    name: 'Item 3',
-  }, {
-    id: 4,
-    name: 'Item 4',
-  }],
-}, {
-  id: 3,
-  name: 'Category 3',
-  items: [{
-    id: 5,
-    name: 'Item 5',
-  }, {
-    id: 6,
-    name: 'Item 6',
-  }],
-}, {
-  id: 4,
-  name: 'Category 4',
-  items: [{
-    id: 7,
-    name: 'Item 7',
-  }, {
-    id: 8,
-    name: 'Item 8',
-  }],
-}, {
-  id: 5,
-  name: 'Category 5',
-  items: [{
-    id: 9,
-    name: 'Item 9',
-  }, {
-    id: 10,
-    name: 'Item 10',
-  }],
-}, {
-  id: 6,
-  name: 'Category 6',
-  items: [{
-    id: 11,
-    name: 'Item 11',
-  }, {
-    id: 12,
-    name: 'Item 12',
-  }],
-}, {
-  id: 7,
-  name: 'Category 7',
-  items: [{
-    id: 13,
-    name: 'Item 13',
-  }, {
-    id: 14,
-    name: 'Item 14',
-  }],
-}, {
-  id: 8,
-  name: 'Category 8',
-  items: [{
-    id: 15,
-    name: 'Item 15',
-  }, {
-    id: 16,
-    name: 'Item 16',
-  }],
-}, {
-  id: 9,
-  name: 'Category 9',
-  items: [{
-    id: 17,
-    name: 'Item 17',
-  }, {
-    id: 18,
-    name: 'Item 18',
-  }],
-}, {
-  id: 10,
-  name: 'Category 10',
-  items: [{
-    id: 19,
-    name: 'Item 19',
-  }, {
-    id: 20,
-    name: 'Item 20',
-  }],
-}];
+const categories = Array.from({ length: 10 }, (_, i) => {
+  const categoryId = i + 1;
+  const items = Array.from({ length: Math.floor(Math.random() * 9) + 2 }, (_, j) => {
+    return {
+      id: (categoryId - 1) * 10 + j + 1,
+      name: `Item ${(categoryId - 1) * 10 + j + 1}`,
+    };
+  });
+
+  return {
+    id: categoryId,
+    name: `Category ${categoryId}`,
+    items,
+  };
+});
 
 const categoryIds = categories.map((category) => category.name);
 </script>
